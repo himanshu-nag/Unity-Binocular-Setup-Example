a1c467dd4fb86d9ccb8628d04309583f81f1bfc2
Himanshu Nag <himanshu_nag@yahoo.com>
2018-07-09 23:08:33 +0530

    Add files via upload

---
+++ Assets/MainScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour {
+	public GameObject image;
+
+	public float zoom;
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.H))
+			SetBinoView ();
+		if (image.activeInHierarchy) {
+			float d = Input.GetAxis("Mouse ScrollWheel");
+			if (d > 0f && zoom<=30)
+			{
+				Camera.main.fieldOfView = zoom++;
+			}
+			else if (d < 0f && zoom>=10)
+			{
+				Camera.main.fieldOfView = zoom--;
+			}
+		}
+	}
+
+	public void SetBinoView(){
+		if (!image.activeInHierarchy) {
+			image.SetActive (true);
+			zoom=Camera.main.fieldOfView = 24;
+		} else {
+			image.SetActive (false);
+			Camera.main.fieldOfView = 60;
+		}
+	}
+}
